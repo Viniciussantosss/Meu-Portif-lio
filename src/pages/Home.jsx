85fc0b1f16f2066fe6e4a2bc6d2a0192ac9b5dab
Viniciussantosss <viniolisan38@gmail.com>
2024-09-14 17:02:25 -0300

    alterações

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 
 function Home(){
-    return (
+ return (
         <>
-        Conteúdo da Home
+        <img src="public/images.png"/>
+        <table border={2}align="center">
+            
+            <thead>
+                <tr>
+                    <h2>
+                    INFORMAÇÕES:
+                    </h2>
+                </tr>
+            </thead>
+        </table>
+
+        <table border={1}>
+        <h5>
+            
+        </h5>
+
+            
+        </table>
         </>
         
+
+
+      
     )
 }
+
+
 
 export default Home;
